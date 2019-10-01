89a1273ef4aa8220077a650d1a2816f79c2158ec
Alexander Levin <alevin.ru@gmail.com>
2019-10-01 08:41:42 +0300

    Create ServicePointList.vue

---
+++ src/components/ServicePointList.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+
+.service-point-list.cell-list
+
+  list-item(
+    v-for= "servicePoint in servicePoints"
+    :key="servicePoint.id"
+    :gray="servicePoint.address"
+    :title="servicePoint.name"
+    @click="$emit('click', servicePoint)"
+  )
+
+</template>
+<script>
+
+const NAME = 'ServicePointList';
+
+export default {
+
+  props: { servicePoints: Array },
+
+  methods: {},
+
+  name: NAME,
+
+};
+
+</script>
+<style scoped lang="scss">
+
+@import "../styles/variables";
+
+</style>
