24f7db8c2529bdba4b668bd05b2cae1e899e4ae1
Alexander Levin <alevin.ru@gmail.com>
2019-10-01 08:41:32 +0300

    Create ListItem.vue

---
+++ src/components/ListItem.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+
+.list-item(@click="$emit('click')")
+  slot(name="title" v-text="title")
+    .title(v-text="title")
+  slot(name="gray")
+    .gray(v-text="gray")
+
+
+</template>
+<script>
+
+const NAME = 'ListItem';
+
+export default {
+  props: {
+    title: String,
+    gray: String,
+  },
+  name: NAME,
+};
+
+</script>
+<style scoped lang="scss">
+
+@import "../styles/variables";
+
+.list-item {
+
+  padding: 8px;
+  text-align: left;
+
+  border-top: $gray-border;
+
+  &:last-child {
+    border-bottom: $gray-border;
+  }
+
+  cursor: pointer;
+
+}
+
+.gray {
+  color: $gray;
+  font-size: small;
+}
+
+</style>
