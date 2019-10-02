0daaeee3f7eb38f58b833b35566c32eaf60a60d6
Alexander Levin <alevin.ru@gmail.com>
2019-10-02 15:19:34 +0300

    Update libs.js

--- src/config/libs.js
+++ src/config/libs.js
@@ -7,7 +7,9 @@ import 'mint-ui/lib/style.css';
 
 import ListItem from '@/components/ListItem.vue';
 import NavHeader from '@/components/NavHeader.vue';
+import ItemsList from '@/components/ItemsList.vue';
 
 Vue.use(Mint);
 Vue.component(NavHeader.name, NavHeader);
+Vue.component(ItemsList.name, ItemsList);
 Vue.component(ListItem.name, ListItem);
