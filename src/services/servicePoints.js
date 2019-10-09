cb689369902d1984958d1a3ca41982dc8f328de8
Alexander Levin <alevin.ru@gmail.com>
2019-10-09 16:06:14 +0300

    loadServicePoints with findByMany

--- src/services/servicePoints.js
+++ src/services/servicePoints.js
@@ -1,12 +1,20 @@
 import ServicePoint from '@/models/ServicePoint';
 import ServiceItem from '@/models/ServiceItem';
+import fpMap from 'lodash/fp/map';
+import uniq from 'lodash/uniq';
+
+import filter from 'lodash/filter';
+
+const mapServicePointId = fpMap('servicePointId');
 
 export async function loadServicePoints(servingMasterId) {
-  await ServiceItem.findAll({
+  const items = await ServiceItem.findAll({
     servingMasterId,
-    limit: 100,
-  }, { with: ['ServicePoint'] });
-  return ServicePoint.getAll();
+    limit: 1000,
+  });
+  const toLoadRelations = filter(items, ({ servicePoint }) => !servicePoint);
+  await ServicePoint.findByMany(mapServicePointId(toLoadRelations));
+  return ServicePoint.getMany(uniq(mapServicePointId(items)));
 }
 
 export function servicePointByIds(ids) {
