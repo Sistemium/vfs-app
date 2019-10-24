188e48a352c08e1fd46d2db72a275a111d394476
Alexander Levin <alevin.ru@gmail.com>
2019-10-25 02:04:17 +0300

    Create SearchInput.vue

---
+++ src/lib/SearchInput.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+
+el-input.search-input(
+  prefix-icon="el-icon-search"
+  v-model="searchText"
+  :clearable="true"
+  :size="size"
+  placeholder="paieška"
+)
+
+</template>
+<script>
+
+import debounce from 'lodash/debounce';
+
+export default {
+
+  name: 'SearchInput',
+
+  props: {
+    value: String,
+    size: {
+      type: String,
+      default: 'mini',
+    },
+    debounce: {
+      type: Number,
+      default: 500,
+    },
+  },
+
+  data() {
+    return { searchText: this.value };
+  },
+
+  // methods: {
+  //   onInput: debounce(function onInput(value) {
+  //     this.$emit('input', value);
+  //   }, 500),
+  // },
+
+  created() {
+    this.$watch('searchText', debounce(value => this.$emit('input', value), this.debounce));
+  },
+
+};
+
+</script>
+<style scoped lang="scss">
+
+@import "../styles/variables";
+
+</style>
