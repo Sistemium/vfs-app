df60f6828901d857df650ec518f3132c8b7d8f90
Alexander Levin <alevin.ru@gmail.com>
2022-09-28 11:52:18 +0300

    config update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
+const { defineConfig } = require('@vue/cli-service');
 const STMManifestPlugin = require('./manifest-plugin');
-
 
 const plugins = [];
 
@@ -9,7 +9,44 @@ if (process.env.NODE_ENV === 'production') {
   }));
 }
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
+  pluginOptions: {
+    // i18n: {
+    //   locale: 'lt',
+    //   fallbackLocale: 'en',
+    //   localeDir: 'locales',
+    //   enableInSFC: true,
+    //   enableBridge: false,
+    // },
+  },
+  configureWebpack: {
+    plugins,
+  },
+  devServer: {
+    allowedHosts: 'all',
+    proxy: {
+      '/api': {
+        target: 'https://vfs.sistemium.com/dapi',
+        // target: 'http://localhost:9390',
+        changeOrigin: true,
+        pathRewrite: {
+          // '^/api/vfs': '/api', // uncomment for localhost api
+          '^/api': '/', // uncomment for vfs.sistemium.com api
+        },
+      },
+      '/ims': {
+        target: 'https://api.sistemium.com/ims',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/ims': '',
+        },
+      },
+    },
+  },
+});
+
+const old_exports = {
   transpileDependencies: [
     'vue2-google-maps',
     'sistemium-debug',
