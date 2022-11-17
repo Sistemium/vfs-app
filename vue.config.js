223b65136af8870c042c79b0181c2ea3c469fe09
Alexander Levin <alevin.ru@gmail.com>
2022-11-17 12:52:07 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: [
+    'vue2-google-maps',
+    'sistemium-debug',
+    'sistemium-dates',
+  ],
   pluginOptions: {
     // i18n: {
     //   locale: 'lt',
@@ -45,48 +49,3 @@ module.exports = defineConfig({
     },
   },
 });
-
-const old_exports = {
-  transpileDependencies: [
-    'vue2-google-maps',
-    'sistemium-debug',
-    'sistemium-dates',
-  ],
-  publicPath: '',
-  pwa: {
-    workboxOptions: {
-      skipWaiting: true,
-    },
-  },
-  configureWebpack: {
-    plugins,
-  },
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://vfs.sistemium.com/dapi',
-        // target: 'http://localhost:9390',
-        changeOrigin: true,
-        pathRewrite: {
-          // '^/api/vfs': '/api', // uncomment for localhost api
-          '^/api': '/', // uncomment for vfs.sistemium.com api
-        },
-      },
-      '/ims': {
-        target: 'https://api.sistemium.com/ims',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/ims': '',
-        },
-      },
-    },
-  },
-  chainWebpack: config => {
-    if (process.env.SAFARI_MODE) {
-      config
-        .output
-        .filename('[name].[hash].js')
-        .end();
-    }
-  },
-};
