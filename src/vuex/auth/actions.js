0ec0b0bd38b868135a12b0439ba1efd14e4c2ac7
Alexander Levin <alevin.ru@gmail.com>
2022-11-17 14:17:35 +0200

    default token

--- src/vuex/auth/actions.js
+++ src/vuex/auth/actions.js
@@ -35,7 +35,7 @@ export default {
     try {
 
       const {
-        id: gotToken,
+        id: gotToken = token,
         account,
         roles,
       } = await (isNative() ? getRoles() : checkRoles(token));
