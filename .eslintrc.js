25072088b39d991b3ca1033b1ae37e5b019bd4d4
Alexander Levin <alevin.ru@gmail.com>
2023-05-24 13:47:19 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     'vue/setup-compiler-macros': true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/essential',
     'plugin:vue-pug/vue3-recommended',
     '@vue/eslint-config-airbnb',
   ],
