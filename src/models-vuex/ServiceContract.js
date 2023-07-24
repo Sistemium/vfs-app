1c70f411aeac449b574a08f8a7ebe24d8218ceda
Alexander Levin <alevin.ru@gmail.com>
2023-07-24 18:05:32 +0300

    Update ServiceContract.js

--- src/models-vuex/ServiceContract.js
+++ src/models-vuex/ServiceContract.js
@@ -26,6 +26,9 @@ export default new VFSDataModel({
       return LegalEntity.reactiveGet(customerLegalEntityId);
     },
     legalType(serviceContract) {
+      if (!serviceContract) {
+        return null;
+      }
       const {
         customerPersonId,
         customerLegalEntityId,
